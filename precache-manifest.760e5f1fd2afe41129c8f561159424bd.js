bcc97d59456c0aa3e71b60b471ee8148372fe24d
ghaith_abuhussain <ghaith_abuhussain@uno-soft.ru>
2025-10-02 02:48:39 +0300

    Fix paths for GitHub Pages for second time

---
+++ precache-manifest.760e5f1fd2afe41129c8f561159424bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c14605a4c6e60a1fa7cc005ad526c25",
+    "url": "/Ghaith-Translator/index.html"
+  },
+  {
+    "revision": "731b13f0ffcaefbed998",
+    "url": "/Ghaith-Translator/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e266eec883dfbc0ffb9a",
+    "url": "/Ghaith-Translator/static/js/2.1a240082.chunk.js"
+  },
+  {
+    "revision": "731b13f0ffcaefbed998",
+    "url": "/Ghaith-Translator/static/js/main.e2bf434a.chunk.js"
+  },
+  {
+    "revision": "5d1b2a87f46bca296df7",
+    "url": "/Ghaith-Translator/static/js/runtime~main.7316ebc4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.693adc52f7116c23144fe4f761ec23d2.js"
+  "/Ghaith-Translator/precache-manifest.760e5f1fd2afe41129c8f561159424bd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Ghaith-Translator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
