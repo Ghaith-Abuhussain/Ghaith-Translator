48da9be4b7cbe74a39a49fefaee9961e80f24579
ghaith_abuhussain <ghaith_abuhussain@uno-soft.ru>
2025-10-02 02:36:29 +0300

    Deploy React app to GitHub Pages

---
+++ precache-manifest.693adc52f7116c23144fe4f761ec23d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeb53caea06a9de2f2ba16077d5a5fe7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "731b13f0ffcaefbed998",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e266eec883dfbc0ffb9a",
+    "url": "/static/js/2.1a240082.chunk.js"
+  },
+  {
+    "revision": "731b13f0ffcaefbed998",
+    "url": "/static/js/main.e2bf434a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
